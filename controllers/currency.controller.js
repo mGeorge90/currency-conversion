612292f9c66757507d04a4652f7251742b8167da
Michael George <111292703+mGeorge90@users.noreply.github.com>
2024-03-19 00:59:53 +0200

    fix covert string small case

--- controllers/currency.controller.js
+++ controllers/currency.controller.js
@@ -4,14 +4,16 @@ const saveUserHistoryJob = require('../jobs/save_user_history.job');
 async function convert(req, res) {
     const exchangeRateAPIKey = process.env.EXCHANGE_RATE_API_KEY;
     try {
-        const { from, to, amount } = req.query;
+        let { from, to, amount } = req.query;
+        from = from.toUpperCase();
+        to = to.toUpperCase();
         const response = await axios.get(`https://v6.exchangerate-api.com/v6/${exchangeRateAPIKey}/latest/${from}`);
         const exchangeRate = response.data?.conversion_rates;
         const exchangeRateTarget = exchangeRate[to];
         if (!exchangeRateTarget) {
             return res.status(400).json({ error: 'Invalid currency' });
         }
-        const convertedAmount = exchangeRateTarget * amount;
+        const convertedAmount =  Math.round(exchangeRateTarget * amount * 100) / 100;
         const userId = req.user.userId;
         const data = {
             from,
