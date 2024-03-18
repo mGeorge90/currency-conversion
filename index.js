c16bd0be05a26ac985ae444f4cabf43da19e1b9e
Michael George <111292703+mGeorge90@users.noreply.github.com>
2024-03-19 00:54:51 +0200

    add swagger docs

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const apiRoutes = require('./apiRoutes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 app.use(express.json());
 app.use('/api', apiRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
